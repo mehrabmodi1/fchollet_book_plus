91eede082da0296467aaf56190fd06faa4e4aee1
mehrabmodi <mehrabmodi@gmail.com>
2022-02-25 14:23:37 -0500

    Put pieces together into a single function that fits the ideal, activating image for any specified unit. Doesn't work with conv2 or conv3 layers though - not sure why. More observations noted in script.

--- convnet_visualization2_maxact_im_for_unit.py
+++ convnet_visualization2_maxact_im_for_unit.py
@@ -7,33 +7,25 @@ Created on Thu Feb 24 21:30:50 2022
 
 This script computes the maximally activating image for a given unit in a given layer. 
 It travels through image space for a given unit.
+
+Note that re-running the fit doesn't generate an identical best-activation image for but an image with very similar feature statistics!!
+This is not a simple offset, which is most noticeable in block 5 units
 """
 
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras import backend as K    #not sure why we aren't just using Numpy
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()      #gradients is deprecated in TF2
+import pdb
 
 model = VGG16(weights = 'imagenet', 
               include_top = False)
 
-layer_name = 'block3_conv1'
-filter_index = 0
-
-layer_output = model.get_layer(layer_name).output
-
-#defining the loss as the mean output of the layer specified in layer_name
-loss = K.mean(layer_output[:, :, :, filter_index])
-
-#getting gradients of loss with small changes in input image
-grads = K.gradients(loss, model.input)[0]      #only index 0 has a non-size1 tensor
-
-#normalizing change made to fitted perfect image in each step by dividing gradients by L2 norm
-grads = grads/(K.sqrt(K.mean(K.square(grads))) + 1e-5)      #adding 1e-5 in the end to prevent divide by 0 numerical error
-
-
-#defining a function that takes an image as input and yeilds loss and gradients of loss
-iterate = K.function([model.input], [loss, grads])
-import numpy as np
-loss_value, grads_value = iterate([np.zeros((1, 150, 150, 3) )])
+#manual input variables
+layer_name = 'block5_conv1'
+filter_index = 4
 
 
 #processing fitted activation image tensor to plot as an image
@@ -49,12 +41,32 @@ def deprocess_image(x):
     return x
 
 
-#main loop that fits the ideal activation image for current unit
-input_img_data = np.random.random((1, 150, 150, 3) ) * 20 + 128.
-step = 1.
+#main function that fits the ideal image to activate the nth unit in the current layer
+def generate_pattern(layer_name, filter_index, size = 150):
+    layer_output = model.get_layer(layer_name).output
 
-for i in range(40):
-    loss_value, grads_value = iterate([input_img_data])     #not sure how this works, K backend function def syntax is unusual
-    input_img_data = input_img_data + grads_value * step
-    step = step + 1
+    #defining the loss as the mean output of the layer specified in layer_name
+    loss = K.mean(layer_output[:, :, :, filter_index])
+    #getting gradients of loss with small changes in input image
+    grads = K.gradients(loss, model.input)[0]      #only index 0 has a non-size1 tensor
+    #normalizing change made to fitted perfect image in each step by dividing gradients by L2 norm
+    grads = grads/(K.sqrt(K.mean(K.square(grads))) + 1e-5)      #adding 1e-5 in the end to prevent divide by 0 numerical error
     
+    #defining a function 'iterate' acc to Keras.backend convention, with specified outputs and inputs, weird syntax
+    iterate = K.function([model.input], [loss, grads])      
+   
+    #initializing a middle-of-range image with some random noise
+    input_img_data = np.random.random((1, size, size, 3) ) * 20 + 128.
+    
+    step = 1.
+    
+    for i in range(40):
+        loss_value, grads_value = iterate([input_img_data])     #not sure how this works, K.backend function def syntax is unusual
+        #pdb.set_trace()
+        input_img_data = input_img_data + grads_value * step
+        step = step + 1     #added this line because otherwise added gradient is decreasing proportionately on every iteration
+    
+    img = input_img_data[0]
+    return deprocess_image(img)
+
+plt.imshow(generate_pattern(layer_name, filter_index))
